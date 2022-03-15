39d153fe50892347c4cc0db32c06bbe6bdb74f4a
georgi.georgiev <ggeorgiev83@gmail.com>
2022-03-15 17:25:33 +0200

    First SpringAdvEx W

---
+++ src/main/java/com/example/springintro/service/BookService.java
@@ -0,0 +1,26 @@
+package com.example.springintro.service;
+
+import com.example.springintro.model.entity.AgeRestriction;
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface BookService {
+    void seedBooks() throws IOException;
+
+    List<Book> findAllBooksAfterYear(int year);
+
+    List<String> findAllAuthorsWithBooksWithReleaseDateBeforeYear(int year);
+
+    List<String> findAllBooksByAuthorFirstAndLastNameOrderByReleaseDate(String firstName, String lastName);
+
+    List<String> findAllBoooksByAgeRestr(AgeRestriction ageRestriction);
+
+    List<Book> findAllByCopiesLessThan(int copies, EditionType editionType);
+
+    List<Book> findByPrice(BigDecimal priceBelow, BigDecimal priceAbove);
+
+}
